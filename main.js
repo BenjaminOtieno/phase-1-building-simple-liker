93a3df48ca24f607412eb61bfdce45da4517604c
BenjaminOtieno <benjamin.otieno@moringaschool.com>
2022-08-27 12:58:06 +0300

    Completed Assignment

--- main.js
+++ main.js
@@ -3,7 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const articleHearts = document.querySelectorAll(".like-glyph");
 
+function likeCallback(event) {
+  const heart = event.target;
+  mimicServerCall("bogusUrl")
+    .then(()=> {
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch(function (error) {
+      const modal = document.querySelector("#modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() => modal.className = "hidden", 3000);
+    });
+}
+
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 
@@ -11,15 +35,15 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
         resolve("Pretend remote server notified of action!");
       }
-    }, 300);
+    }, 3000);
   });
 }
